4d4b84c301949130d9e95769209cbf5a3cb85f79
Andrew <awalden0808@gmail.com>
2015-11-23 13:36:18 -0500

    Added Dat GUI

--- js/code.js
+++ js/code.js
@@ -1,24 +1,71 @@
 var synth = null;
-var wave = null;
-var mul = null;
-var poly = null;
-var attack = null;
-var release = null;
-var hold = null;
-var decay = null;
-var level = null;
-var fontNum = null;
-var soundfont = null;
+
+var oscillator1;
+var oscillator2;
+var envelope;
+var soundfont;
+var general;
+
+var fontFolder;
+var fontControl;
+
 var mode = 0;
+
 var keydict;
 var midicps;
 
-function octaveUp() {
-  document.getElementById("octave").stepUp();
+var instrumentTypes = {"All Instruments": 16,
+                       "Piano": 0,
+                       "Chromatic Percussion": 1,
+                       "Organ": 2,
+                       "Guitar": 3,
+                       "Bass": 4,
+                       "Strings": 5,
+                       "Ensemble": 6,
+                       "Brass": 7,
+                       "Reed": 8,
+                       "Pipe": 9,
+                       "Synth Lead": 10,
+                       "Synth Pad": 11,
+                       "Synth Effects": 12,
+                       "Ethnic": 13,
+                       "Percussive": 14,
+                       "Sound Effects": 15};
+
+var instrumentTypeList = [];
+for (type in instrumentTypes) {
+  instrumentTypeList.push(type);
 }
 
-function octaveDown() {
-  document.getElementById("octave").stepDown();
+var midiInstruments = ["Acoustic Grand Piano", "Bright Acoustic Piano", "Electric Grand Piano", "Honky-tonk Piano","Electric Piano 1", "Electric Piano 2", "Harpsichord", "Clavinet", "Celesta", "Glockenspiel", "Music Box", "Vibraphone", "Marimba", "Xylophone", "Tubular Bells", "Dulcimer", "Drawbar Organ", "Percussive Organ", "Rock Organ", "Church Organ", "Reed Organ", "Accordion", "Harmonica", "Tango Accordion", "Acoustic Guitar (nylon)", "Acoustic Guitar (steel)", "Electric Guitar (jazz)", "Electric Guitar (clean)", "Electric Guitar (muted)", "Overdriven Guitar", "Distortion Guitar", "Guitar Harmonics", "Acoustic Bass", "Electric Bass (finger)", "Electric Bass (pick)", "Fretless Bass", "Slap Bass 1", "Slap Bass 2", "Synth Bass 1", "Synth Bass 2", "Violin", "Viola", "Cello", "Contrabass", "Tremolo Strings", "Pizzicato Strings", "Orchestral Harp", "Timpani", "String Ensemble 1", "String Ensemble 2", "Synth Strings 1", "Synth Strings 2", "Choir Aahs", "Voice Oohs", "Synth Choir", "Orchestra Hit", "Trumpet", "Trombone", "Tuba", "Muted Trumpet", "French Horn", "Brass Section", "Synth Brass 1", "Synth Brass 2", "Soprano Sax", "Alto Sax", "Tenor Sax", "Baritone Sax", "Oboe", "English Horn", "Bassoon", "Clarinet", "Piccolo", "Flute", "Recorder", "Pan Flute", "Blown bottle", "Shakuhachi", "Whistle", "Ocarina", "Lead 1 (square)", "Lead 2 (sawtooth)", "Lead 3 (calliope)", "Lead 4 chiff", "Lead 5 (charang)", "Lead 6 (voice)", "Lead 7 (fifths)", "Lead 8 (bass + lead)", "Pad 1 (new age)", "Pad 2 (warm)", "Pad 3 (polysynth)", "Pad 4 (choir)", "Pad 5 (bowed)", "Pad 6 (metallic)", "Pad 7 (halo)", "Pad 8 (sweep)", "FX 1 (rain)", "FX 2 (soundtrack)", "FX 3 (crystal)", "FX 4 (atmosphere)", "FX 5 (brightness)", "FX 6 (goblins)", "FX 7 (echoes)", "FX 8 (sci-fi)", "Sitar", "Banjo", "Shamisen", "Koto", "Kalimba", "Bagpipe", "Fiddle", "Shanai", "Tinkle Bell", "Agogo", "Steel Drums", "Woodblock", "Taiko Drum", "Melodic Tom", "Synth Drum", "Reverse Cymbal", "Guitar Fret Noise", "Breath Noise", "Seashore", "Bird Tweet", "Telephone Ring", "Helicopter", "Applause", "Gunshot"];
+
+
+var General = function () {
+  this.poly = 4;
+  this.octave = 4;
+};
+
+var Oscillator = function () {
+  this.wave = "sin";
+  this.mul = 0.25;
+  this.offset = 0;
+};
+
+var Envelope = function () {
+  this.attack = 50;
+  this.release = 300;
+  this.sustain = 0.5;
+  this.hold = 0;
+  this.decay = 300;
+  this.level = 1;
+};
+
+
+var SoundFont = function () {
+  this.instrumentType = instrumentTypeList[0];
+  this.instrument = midiInstruments[0];
+  this.midiMode = false;
+  this.length = 64;
 }
 
 function pressKey(midi) {
@@ -39,6 +86,7 @@ function pressKey(midi) {
 
 function unpressKey(midi) {
   midi -= 48;
+
   var key = document.getElementById("key" + midi.toString());
   if (key != null) {
     var keyClass = key.getAttribute("class");
@@ -49,28 +97,72 @@ function unpressKey(midi) {
 }
 
 function stop(midi) {
-  if (mode == 0) {
+  if (!soundfont.midiMode) {
     synth.noteOff(midi, 100);
   }
 }
 
 function play(midi) {
-  if (mode == 0) {
-    var freq = midicps.at(midi);
-    console.log(midi);
-    synth.noteOnWithFreq(freq, 100);
+  if (soundfont.midiMode) {
+    T.soundfont.play(midi);
   }
   else {
-    T.soundfont.play(midi);
+    var freq = midicps.at(midi);
+    synth.noteOnWithFreq(freq, 100);
   }
 }
 
 function octaveShift(midi) {
   midi -= 48;
-  var octaveRange = document.getElementById("octave");
-  var octave = Number(octaveRange.value);
-  midi += octave * 12;
+  midi += general.octave * 12;
   return midi;
+}
+
+function checkNewSynth(value) {
+  if (soundfont.midiMode) {
+    T.soundfont.setInstrument(midiInstruments.indexOf(soundfont.instrument));
+  }
+  else {
+
+    synth = T("SynthDef").play();
+
+    synth.def = function(opts) {
+      var osc1, osc2, env;
+      var freq1 = opts.freq;
+      var freq2 = opts.freq;
+      if (oscillator1.offset != 0) {
+        freq1 = freq1 * Math.pow(2, oscillator1.offset / 12);
+      }
+      if (oscillator2.offset != 0) {
+        freq2 = freq2 * Math.pow(2, oscillator2.offset / 12);
+      }
+      osc1 = T(oscillator1.wave, {freq:freq1, mul: (oscillator1.mul / 2)});
+      osc2 = T(oscillator2.wave, {freq:freq2, mul: (oscillator2.mul / 2)});
+      env = T("ahdsfr", {a: envelope.attack, r: envelope.release, h: envelope.hold, d: envelope.decay, s: envelope.sustain, lv: envelope.level}, osc1, osc2);
+      env.plot({target:wavePlot});
+      return env.on("ended", opts.doneAction).bang();
+    };
+    synth.poly = general.poly;
+
+  }
+}
+
+function preloadSoundfont() {
+  checkNewSynth();
+  if (mode == 1) {
+    var loadingStatus = document.getElementById("loading-status");
+    loadingStatus.innerHTML = "Preloading soundfont...";
+    T.soundfont.emptyCache();
+    var noteArray = [];
+    for (var i = 0; i < 128; i++) {
+      noteArray.push(i);
+    }
+    T.soundfont.preload(noteArray);
+    loadingStatus.innerHTML = "Done.";
+  }
+  else {
+
+  }
 }
 
 var blacks = [1, 3, 6, 8, 10, 13, 15, 18, 20, 22, 25, 27];
@@ -92,112 +184,83 @@ function generatePiano() {
   piano.appendChild(keys);
 }
 
-var soundfontList = ["Acoustic Grand Piano", "Bright Acoustic Piano", "Electric Grand Piano", "Honky-tonk Piano","Electric Piano 1", "Electric Piano 2", "Harpsichord", "Clavinet", "Celesta", "Glockenspiel", "Music Box", "Vibraphone", "Marimba", "Xylophone", "Tubular Bells", "Dulcimer", "Drawbar Organ", "Percussive Organ", "Rock Organ", "Church Organ", "Reed Organ", "Accordion", "Harmonica", "Tango Accordion", "Acoustic Guitar (nylon)", "Acoustic Guitar (steel)", "Electric Guitar (jazz)", "Electric Guitar (clean)", "Electric Guitar (muted)", "Overdriven Guitar", "Distortion Guitar", "Guitar Harmonics", "Acoustic Bass", "Electric Bass (finger)", "Electric Bass (pick)", "Fretless Bass", "Slap Bass 1", "Slap Bass 2", "Synth Bass 1", "Synth Bass 2", "Violin", "Viola", "Cello", "Contrabass", "Tremolo Strings", "Pizzicato Strings", "Orchestral Harp", "Timpani", "String Ensemble 1", "String Ensemble 2", "Synth Strings 1", "Synth Strings 2", "Choir Aahs", "Voice Oohs", "Synth Choir", "Orchestra Hit", "Trumpet", "Trombone", "Tuba", "Muted Trumpet", "French Horn", "Brass Section", "Synth Brass 1", "Synth Brass 2", "Soprano Sax", "Alto Sax", "Tenor Sax", "Baritone Sax", "Oboe", "English Horn", "Bassoon", "Clarinet", "Piccolo", "Flute", "Recorder", "Pan Flute", "Blown bottle", "Shakuhachi", "Whistle", "Ocarina", "Lead 1 (square)", "Lead 2 (sawtooth)", "Lead 3 (calliope)", "Lead 4 chiff", "Lead 5 (charang)", "Lead 6 (voice)", "Lead 7 (fifths)", "Lead 8 (bass + lead)", "Pad 1 (new age)", "Pad 2 (warm)", "Pad 3 (polysynth)", "Pad 4 (choir)", "Pad 5 (bowed)", "Pad 6 (metallic)", "Pad 7 (halo)", "Pad 8 (sweep)", "FX 1 (rain)", "FX 2 (soundtrack)", "FX 3 (crystal)", "FX 4 (atmosphere)", "FX 5 (brightness)", "FX 6 (goblins)", "FX 7 (echoes)", "FX 8 (sci-fi)", "Sitar", "Banjo", "Shamisen", "Koto", "Kalimba", "Bagpipe", "Fiddle", "Shanai", "Tinkle Bell", "Agogo", "Steel Drums", "Woodblock", "Taiko Drum", "Melodic Tom", "Synth Drum", "Reverse Cymbal", "Guitar Fret Noise", "Breath Noise", "Seashore", "Bird Tweet", "Telephone Ring", "Helicopter", "Applause", "Gunshot"];
-
-function loadSoundfontMenu() {
-  var soundfontTypeSel = document.getElementById("soundfont-type");
-  var soundfontType = Number(soundfontTypeSel.options[soundfontTypeSel.selectedIndex].value);
-  console.log(soundfontType);
-
-  var count = 8;
-  if (soundfontType == 16) {
-    count = soundfontList.length;
-    soundfontType = 0;
-  }
-  var soundfontSel = document.getElementById("soundfont");
-  soundfontSel.innerHTML = "";
-  for (var i = (soundfontType * count); i < ((soundfontType + 1) * count); i++) {
-    var option = document.createElement("option");
-    option.setAttribute("value", i.toString());
-    option.innerHTML = soundfontList[i];
-    soundfontSel.appendChild(option);
+function octaveUp() {
+  if (general.octave < 9) {
+    general.octave++;
   }
 }
 
-function checkNewSynth() {
-  var loadingStatus = document.getElementById("loading-status");
-  loadingStatus.innerHTML = "Preparing synth...";
-
-  var waveSel = document.getElementById("wave");
-  var newWave = waveSel.options[waveSel.selectedIndex].value;
-
-  var fontNumSel = document.getElementById("soundfont");
-  var newFontNum = Number(fontNumSel.options[fontNumSel.selectedIndex].value);
-
-  var mulRange = document.getElementById("mul");
-  var newMul = Number(mulRange.value);
-
-  var polyRange = document.getElementById("poly");
-  var newPoly = Number(polyRange.value);
-
-  var attackRange = document.getElementById("attack");
-  var newAttack = Number(attackRange.value);
-
-  var releaseRange = document.getElementById("release");
-  var newRelease = Number(releaseRange.value);
-
-  var holdRange = document.getElementById("hold");
-  var newHold = Number(holdRange.value);
-
-  var decayRange = document.getElementById("decay");
-  var newDecay = Number(decayRange.value);
-
-  var levelRange = document.getElementById("level");
-  var newLevel = Number(levelRange.value);
-
-  if ((newWave !== wave) || (newFontNum !== fontNum) || (newMul !== mul) || (newPoly !== poly) || (newAttack !== attack) || (newRelease !== release) || (newHold !== hold)
-      || (newDecay !== decay) || (newLevel !== level)) {
-    if (synth != null) {
-      synth.allNoteOff();
-    }
-    wave = newWave;
-    mul = newMul;
-    poly = newPoly;
-    attack = newAttack;
-    release = newRelease;
-    hold = newHold;
-    decay = newDecay;
-    level = newLevel;
-    fontNum = newFontNum;
-    if (wave === "soundfont") {
-      mode = 1;
-      T.soundfont.setInstrument(fontNum);
-    }
-    else {
-      mode = 0;
-      var env = T("ahdsfr", {a: attack, r: release, h: hold, d: decay, lv: level});
-      env.plot({target:wavePlot});
-      synth = T("OscGen", {wave: wave, env: env, mul: mul}).play();
-      synth.poly = poly;
-    }
+function octaveDown() {
+  if (general.octave > 0) {
+    general.octave--;
   }
-  loadingStatus.innerHTML = "Done.";
 }
 
-function preloadSoundfont() {
-  checkNewSynth();
-  if (mode == 1) {
-    var loadingStatus = document.getElementById("loading-status");
-    loadingStatus.innerHTML = "Preloading soundfont...";
-    T.soundfont.emptyCache();
-    var noteArray = [];
-    for (var i = 0; i < 128; i++) {
-      noteArray.push(i);
-    }
-    T.soundfont.preload(noteArray);
-    loadingStatus.innerHTML = "Done.";
+function changeInstrumentType(value) {
+  fontFolder.remove(fontControl);
+  var channel = instrumentTypes[value];
+  var options = midiInstruments;
+  if (channel < 16) {
+    var typeCount = 8;
+    options = midiInstruments.slice(typeCount * channel, (channel + 1) * typeCount);
   }
-  else {
-
-  }
+  fontControl = fontFolder.add(soundfont, 'instrument', options);
+  fontControl.onFinishChange(checkNewSynth);
 }
 
 window.onload = function () {
+
+  var waves = ["sin", "cos", "pulse", "tri", "saw", "fami", "konami", "+sin", "+pulse", "+tri", "+saw", "fnoise", "soundfont"];
+
+  general = new General();
+  oscillator1 = new Oscillator();
+  oscillator2 = new Oscillator();
+  envelope = new Envelope();
+  soundfont = new SoundFont();
+
+  var gui = new dat.GUI();
+
+  var polyControl = gui.add(general, 'poly', 1, 64);
+  polyControl.step(1);
+  polyControl.onFinishChange(checkNewSynth);
+  var octaveControl = gui.add(general, 'octave', 0, 9);
+  octaveControl.onFinishChange(function (value) {});
+  octaveControl.step(1);
+  octaveControl.listen();
+
+  var f1 = gui.addFolder('Oscillator 1');
+
+  f1.add(oscillator1, 'wave', waves).onFinishChange(checkNewSynth);
+  f1.add(oscillator1, 'mul', 0, 1).onFinishChange(checkNewSynth);
+  f1.add(oscillator1, 'offset', -12, 12).onFinishChange(checkNewSynth);
+
+  var f2 = gui.addFolder('Oscillator 2');
+  f2.add(oscillator2, 'wave', waves).onFinishChange(checkNewSynth);
+  f2.add(oscillator2, 'mul', 0, 1).onFinishChange(checkNewSynth);
+  f2.add(oscillator2, 'offset', -12, 12).onFinishChange(checkNewSynth);
+
+  var f3 = gui.addFolder('Envelope');
+  f3.add(envelope, 'attack', 0, 2000).onFinishChange(checkNewSynth);
+  f3.add(envelope, 'release', 0, 2000).onFinishChange(checkNewSynth);
+  f3.add(envelope, 'sustain', 0, 1).onFinishChange(checkNewSynth);
+  f3.add(envelope, 'hold', 0, 2000).onFinishChange(checkNewSynth);
+  f3.add(envelope, 'decay', 0, 2000).onFinishChange(checkNewSynth);
+  f3.add(envelope, 'level', 0, 1).onFinishChange(checkNewSynth);
+
+  fontFolder = gui.addFolder('MIDI');
+  fontFolder.add(soundfont, 'midiMode').onFinishChange(checkNewSynth);
+  fontFolder.add(soundfont, 'length', 1, 256).onFinishChange(checkNewSynth);
+  fontFolder.add(soundfont, 'instrumentType', instrumentTypeList).onFinishChange(changeInstrumentType);
+  fontControl = fontFolder.add(soundfont, 'instrument', midiInstruments);
+  fontControl.onFinishChange(checkNewSynth);
+
+
   keydict = T("ndict.key");
   midicps = T("midicps");
 
-  loadSoundfontMenu();
   generatePiano();
+
+  checkNewSynth(0);
 
   T("keyboard").on("keydown", function(e) {
     if (e.keyCode == 189) {
@@ -219,7 +282,6 @@ window.onload = function () {
       octaveUp();
     }
     var midi = keydict.at(e.keyCode);
-    checkNewSynth();
     if (midi) {
       pressKey(midi);
       midi = octaveShift(midi);
@@ -227,7 +289,6 @@ window.onload = function () {
     }
   }).on("keyup", function(e) {
     var midi = keydict.at(e.keyCode);
-    checkNewSynth();
     if (midi) {
       unpressKey(midi);
       midi = octaveShift(midi);
